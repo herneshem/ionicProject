81539f00c81f925bece2135f59503d666b2b5b74
hernest <ernestoami@hotmail.com>
2025-01-21 13:24:05 +0100

    IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonInput, IonItem, IonList, IonLabel, IonSegment, IonSegmentButton, MenuController, IonMenu, IonButton} from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonList, IonHeader, IonLabel, IonSegment, IonSegmentButton, IonToolbar, IonMenu, IonButton, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private menuCtrl: MenuController) {}
+
+    Altura:number=0;
+    Peso:number=0
+    resultado:number=0
+
+    calcularIMC(){
+      console.log(this.Peso/(this.Altura * this.Altura) )
+      this.resultado=this.Peso/(this.Altura * this.Altura)
+
+    }
+
+  openFirstMenu() {
+
+    this.menuCtrl.open('first-menu');
+  }
+
+  openSecondMenu() {
+
+    this.menuCtrl.open('second-menu');
+  }
+
+  openEndMenu() {
+
+    this.menuCtrl.open('end');
+  }
 }
